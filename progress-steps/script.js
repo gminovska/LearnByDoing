d2b83b0e17572bfded5f71cdb6ea2b49765bb8d8
Gordana Minovska <gordanam@fileonq.com>
2022-08-11 13:13:56 -0700

    Add functionality on prev and next btn click

---
+++ progress-steps/script.js
@@ -0,0 +1,41 @@
+// global counter variable to keep track of state
+let activeStep = 1;
+
+//get the btn and attach event handlers
+let prevBtn = document.getElementById("prev");
+prevBtn.addEventListener("click", handlePrevClick);
+
+let nextBtn = document.getElementById("next");
+nextBtn.addEventListener("click", handleNextClick);
+
+// get the steps, lines  to change their properties on click
+let steps = document.querySelectorAll(".step");
+let lines = document.querySelectorAll(".line");
+
+function handlePrevClick() {
+  activeStep--;
+  steps[activeStep].classList.remove("active");
+  lines[activeStep - 1].classList.remove("active");
+  if (activeStep == 1) {
+    // enable prev btn
+    prevBtn.disabled = true;
+  }
+  if (activeStep < steps.length) {
+    nextBtn.disabled = false;
+  }
+}
+
+function handleNextClick() {
+  steps[activeStep].classList.add("active");
+  lines[activeStep - 1].classList.add("active");
+  activeStep++;
+
+  if (activeStep == steps.length) {
+    //disable next btn
+    nextBtn.disabled = true;
+  }
+  if (activeStep > 1) {
+    // enable prev btn
+    prevBtn.disabled = false;
+  }
+}
