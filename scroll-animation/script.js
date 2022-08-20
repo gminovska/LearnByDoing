6f0f86b39021e59f68153181ecd85b677cb9344e
Gordana Minovska <gordanam@fileonq.com>
2022-08-19 21:11:10 -0700

    Performance optimization w requestAnimationFrame

--- scroll-animation/script.js
+++ scroll-animation/script.js
@@ -2,16 +2,28 @@
 //get all the boxes
 let boxes = document.querySelectorAll('.box');
 
-//listen for scroll events and read the scroll position 
-// What is the difference between document vs window addEventListener?
-document.addEventListener('scroll', boxVisibility)
+//call the function for the initial box rendering, prior to scrolling
+boxVisibility();
 
+// There is no notable difference in the behavior. 
+// The difference is that the event handler won’t get called faster than the frame rate as determined by the requestAnimationFrame method. 
+// This means that your event handler code will get called around 60 times a second - Creating Web Animations by Kirupa Chinnathambi
 
+var isScrolling = false;
+
+window.addEventListener("scroll", throttleScroll);
+
+function throttleScroll() {
+    if (isScrolling == false) {
+        requestAnimationFrame(function () {
+            boxVisibility();
+            isScrolling = false;
+        });
+    }
+    isScrolling = true;
+}
 function boxVisibility() {
     // set a triggerLine at 80% of the screen. Add visible class to all the elements above it
     const triggerLine = window.innerHeight / 5 * 4;
     boxes.forEach((box) => box.getBoundingClientRect().top < triggerLine ? box.classList.add('visible') : box.classList.remove('visible'))
 }
-
-//call the function for the initial box rendering, prior to scrolling
-boxVisibility();
