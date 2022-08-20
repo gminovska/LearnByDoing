c60dbbd7da05f49e689a52d03a46355aa5a9f3a5
Gordana Minovska <gordanam@fileonq.com>
2022-08-19 19:28:40 -0700

    Re-implement js logic to avoid using forEach loop

--- scroll-animation/script.js
+++ scroll-animation/script.js
@@ -4,11 +4,11 @@ const screenHeight = window.innerHeight;
 const boxHeight = 220; // 200px height + 20px bottom margin
 
 //the first box is always visible, regardless of screen height. That is why if numberOfBoxes = 0, we default it to 1.
-let numberOfBoxes = Math.floor(screenHeight / boxHeight) || 1;
-console.log(numberOfBoxes);
+let numberOfBoxes = Math.floor((screenHeight + window.scrollY) / boxHeight) || 1;
+console.log(numberOfBoxes, window.scrollY);
 
 //get all the boxes
-let boxes = document.querySelectorAll('.box');
+let boxes = document.getElementsByClassName('box');
 
 // initially render all the boxes that fit into the screen height
 for (let i = 0; i < numberOfBoxes; i++) {
@@ -17,22 +17,19 @@ for (let i = 0; i < numberOfBoxes; i++) {
 
 //listen for scroll events and read the scroll position 
 document.addEventListener('scroll', (e) => {
+    const newNumberOfBoxes = Math.floor((screenHeight + window.scrollY) / boxHeight);
+    // boxes.forEach((box, index) => index < newNumberOfBoxes ? box.classList.add('visible') : box.classList.remove('visible'))
 
+    if (newNumberOfBoxes > numberOfBoxes) {
+        // add visible class to the next box
+        boxes[newNumberOfBoxes - 1].classList.add('visible');
 
-    const newNumberOfBoxes = Math.floor((screenHeight + window.scrollY) / boxHeight) - 1;
-    boxes.forEach((box, index) => index < newNumberOfBoxes ? box.classList.add('visible') : box.classList.remove('visible'))
-    // if (newNumberOfBoxes < boxes.length) {
-    //     if (newNumberOfBoxes > numberOfBoxes) {
-    //         // add visible class to the next box
-    //         boxes[newNumberOfBoxes - 1].classList.add('visible');
-    //         numberOfBoxes = newNumberOfBoxes;
-    //     }
-    //     if (newNumberOfBoxes < numberOfBoxes) {
-    //         //remove visible class from the last box
-    //         boxes[newNumberOfBoxes].classList.remove('visible');
-    //     }
-    // }
+        numberOfBoxes = newNumberOfBoxes;
+    }
+    if (newNumberOfBoxes < numberOfBoxes) {
+        //remove visible class from the last box
+        boxes[newNumberOfBoxes].classList.remove('visible');
+        numberOfBoxes = newNumberOfBoxes;
 
-    console.log("boxes visible ", window.scrollY)
-
+    }
 })
